39bcf5a54695cb59fc5edcd0cfe3c662a137922e
raff0z <razorzeta@gmail.com>
2014-06-13 10:47:39 +0200

    test code

--- src/it/uniroma3/vi/helper/HelperTransaction.java
+++ src/it/uniroma3/vi/helper/HelperTransaction.java
@@ -10,16 +10,18 @@ public class HelperTransaction {
     public String blobHashToString(Blob blob) throws SQLException, IOException {
 
 	String hash = "";
+	//TODO FIX
+	if (blob != null) {
+	    InputStream is = blob.getBinaryStream();
 
-	InputStream is = blob.getBinaryStream();
-
-	int ch;
-	while ((ch = is.read()) != -1) {
-	    String part = Integer.toHexString(ch);
-	    if (part.length() == 1)
-		hash += "0" + part;
-	    else
-		hash += part;
+	    int ch;
+	    while ((ch = is.read()) != -1) {
+		String part = Integer.toHexString(ch);
+		if (part.length() == 1)
+		    hash += "0" + part;
+		else
+		    hash += part;
+	    }
 	}
 	return hash;
     }
